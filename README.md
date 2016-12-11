60c96056a2a99f1e59bdca13884c55bb27a1546e
Daniel McLaughlin <danielsmc@gmail.com>
2016-12-11 11:31:34 -0500

    Don't worry about editing the gulpfile anymore

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ _This is experimental code. Please don't use it for anything important._
 
 Gulp-hammerspoon relays the status of your [Gulp](http://gulpjs.com) tasks to [Hammerspoon](http://www.hammerspoon.org). The code in `gulp-hammerspoon.lua` can display the status in your menubar, or you can use it as a reference to write whatever script you want.
 
-To get notifications from a project, you need to npm install gulp-hammerspoon and add `require('gulp-hammerspoon')()` to the gulpfile. This approach is totally backwards, since we actually want this for every instance of gulp on this Mac without mucking with each project. I suspect the right way is to make a global cli wrapper `gulp-hs` that calls gulp with the hooks added. Stay tuned.
+To get notifications from a gulp process, run it as `gulp --require gulp-hammerspoon <taskname>`. You could alias that to `gulp-hs` or even `gulp` if you're feeling saucy. Unfortunately, you'll still have to install gulp-hammerspoon locally for every project. If you don't want to bother your collaborators who aren't hip to Hammerspoon, you don't have to save the dependency in package.json.
--- index.js
+++ index.js
@@ -5,27 +5,26 @@ var onExit = require('signal-exit');
 var path = require('path');
 var tildify = require('tildify');
 
-module.exports = function() {
-  const projectName = tildify(path.resolve(__dirname).split('/node_modules')[0]);
 
-  function send(params) {
-    params.project = projectName;
-    const url = "hammerspoon://gulp?"+querystring.stringify(params);
-    openurl.open(url, e => (false)); // Fail silently if hammerspoon urls don't work
+const projectName = tildify(path.resolve(__dirname).split('/node_modules')[0]);
+
+function send(params) {
+  params.project = projectName;
+  const url = "hammerspoon://gulp?"+querystring.stringify(params);
+  openurl.open(url, e => (false)); // Fail silently if hammerspoon urls don't work
+}
+
+onExit(() => send({event: "quit"}));
+
+gulp.onAll(e => {
+  const params = {
+    task: e.task,
+    event: {task_start:"start", task_stop:"stop", task_err:"err"}[e.src]
   }
-
-  onExit(() => send({event: "quit"}));
-
-  gulp.onAll(e => {
-    const params = {
-      task: e.task,
-      event: {task_start:"start", task_stop:"stop", task_err:"err"}[e.src]
-    }
-    if (e.err) {
-      params.err = e.err.message;
-    }
-    if (params.event) {
-      send(params);
-    }
-  });
-};
+  if (e.err) {
+    params.err = e.err.message;
+  }
+  if (params.event) {
+    send(params);
+  }
+});
